acdd25c1531a40d85e334e370336c1d81539ab08
Rapiwash <sistemarapiwash@gmail.com>
2024-05-26 21:39:37 -0500

    correciones

--- src/services/global.js
+++ src/services/global.js
@@ -1,7 +1,7 @@
 // Zona Horaria
 export const timeZone = "America/Lima";
 // Factura
-export const showFactura = false;
+export const showFactura = true;
 // Tipo de Moneda
 export const tipoMoneda = "PEN";
 // Formato de la moneda (separacion de miles - decimales) segun la configuracion regional
